5995b52d7ba49dd22c3a6ae14da1dee47886dcbe
Nick Jarvis <najarvis2016@gmail.com>
2017-05-30 16:18:17 +0200

    Working, but not working

--- mid_dis.py
+++ mid_dis.py
@@ -1,29 +1,79 @@
+import sys
 import ctypes
-nDLL = ctypes.CDLL("mid_dis_dll.dll")
-
-N = 9
-
-func = nDLL['new_map']
-func.restype = ctypes.c_double * (2 ** N + 1) * (2 ** N + 1)
-func.argtype = ctypes.c_int
-
-array = func(N)
-print(array)
-width = len(array)
-print(width)
-
 import pygame
+pygame.init()
+
+def get_func():
+    nDLL = ctypes.CDLL("mid_dis_dll.dll")
+
+    N = 8 # 2^9 = 512. Creates a 513 x 513 map
+    func = nDLL['new_map']
+    func.restype = ctypes.POINTER(ctypes.c_double * (2 ** N + 1) * (2 ** N + 1))
+    #func.restype = ctypes.c_double * (2 ** N + 1) * (2 ** N + 1)
+    func.argtype = ctypes.c_int
+
+    return func
+
+def run():
+    N = 9
+    func = get_func()
+    array = func(N)
+
+    print(array)
+    width = len(array[0])
+
+    surf = pygame.Surface((width, width))
+
+    print("locking")
+    surf.lock()
+    print("going into the loop")
+    for y in xrange(width):
+        for x in xrange(width):
+            print(y, x)
+            try:
+                clr = max(min(int(array[0][x][y] * 256), 255), 0)
+            except ValueError:
+                print array[0][x][x]
+                clr = 0
+            except OverflowError:
+                print array[0][x][y]
+                clr = 255
+
+            surf.set_at((x, y), (clr, clr, clr))
+    print("done with the loop")
+    surf.unlock()
+    print("unlocked")
+
+    pygame.image.save(surf, "mid_dis.png")
+
+def trace(frame, event, arg):
+    print "%s, %s:%d" % (event, frame.f_code.co_filename, frame.f_lineno)
+    return trace
+
+sys.settrace(trace)
+run()
+
+"""
+import pygame
+pygame.init()
 surf = pygame.Surface((width, width))
 
+print("locking")
 surf.lock()
+print("going into the loop")
 for y in xrange(width):
+    print("y=%d"%y)
     for x in xrange(width):
+        print ("x=%d"%x)
         try:
-            clr = max(min(int(array[y][x]), 255), 0)
+            clr = max(min(int(array[1][x][y] * 256), 255), 0)
         except ValueError:
-            print array[y][x]
+            print array[0][y][x]
             clr = 0
         surf.set_at((x, y), (clr, clr, clr))
+print("done with the loop")
 surf.unlock()
+print("unlocked")
 
 pygame.image.save(surf, "mid_dis.png")
+"""
