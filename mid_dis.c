e80919661df906b2055de352a8cfb845a7d4b6af
Nick Jarvis <najarvis2016@gmail.com>
2017-05-31 10:47:09 +0200

    Works, weird symetric horizontal lines bugging me.

--- mid_dis.c
+++ mid_dis.c
@@ -8,66 +8,149 @@ double rd(){
 }
 
 double rand_h(int r){
-    return (double) (2 * (rd() - 0.5)) / pow(2, r+1);
+    return (double) (2 * (rd() - 0.5)) / pow(2, r + 1);
 }
 
-void diamond(double** array, int r_depth, int x, int y, int N){
+void diamond(double* array, int r_depth, int x, int y, int N){
+    // Here is where the problem lies, I believe. This is the process
+    // that adds detail to the array based on the current detail and
+    // recursion depth level (although this isn't explicitly recursive
+    // because it doesn't call itself, it is taken from an algorithm that
+    // is).
+
+    // The size of the square to which detail will be added
     int size = (int) (pow(2, N) / pow(2, r_depth));
 
-    double tl = array[size * x][size * y];
-    double tr = array[size * x + size][size * y];
-    double br = array[size * x + size][size * y + size];
-    double bl = array[size * x][size * y + size];
+    // The size of the whole map
+    int full_size = (int) pow(2, N) + 1;
 
-    array[size * x + size / 2][size * y + size / 2] = ((tl + tr + br + bl) / 4) + rand_h(r_depth + 1);
+    #ifdef DEBUG
+    printf("Whole size: %d, Step size: %d, tl location: %d\n", full_size, size, full_size * (y * size) + (x * size));
+    #endif
+
+    double tl = array[full_size * (y * size) + (x * size)];
+    double tr = array[full_size * (y * size) + (x * size) + size];
+    double br = array[full_size * ((y * size) + size) + (x * size) + size];
+    double bl = array[full_size * ((y * size) + size) + (x * size)];
 
     // Square step
-    
-    double mid = array[size * x + size / 2][size * y + size / 2];
+    // | 0.5 -> 0.6 <- 0.5 |
+    // |  |             |  |
+    // |  v             v  |
+    // | 0.4           0.3 |
+    // |  ^             ^  |
+    // |  |             |  |
+    // | 0.5 -> 0.9 <- 0.5 |
+
+    double mid = array[full_size * ((y * size) + size / 2) + (x * size) + size / 2];
 
     // Top mid
-    array[size * x + size / 2][size * y] = (tl + tr + mid) / 3.0 + rand_h(r_depth);
+    array[full_size * ((y * size) + size) + (x * size) + size / 2] = (tl + tr) / 2.0 + rand_h(r_depth);
 
     // Right mid
-    array[size * x + size][size * y + size / 2] = (tr + br + mid) / 3.0 + rand_h(r_depth);
+    array[full_size * ((y * size) + size / 2) + (x * size) + size] = (tr + br) / 2.0 + rand_h(r_depth);
 
     // Bottom mid
-    array[size * x + size / 2][size * y + size] = (bl + br + mid) / 3.0 + rand_h(r_depth);
+    array[full_size * ((y * size) + size) + (x * size) + size / 2] = (bl + br) / 2.0 + rand_h(r_depth);
 
     // Left mid
-    array[size * x + size][size * y + size / 2] = (tl + bl + mid) / 3.0 + rand_h(r_depth);
+    array[full_size * ((y * size) + size / 2) + (x * size) + size] = (tl + bl) / 2.0 + rand_h(r_depth);
+    
+    // Center of the current section
+
+    // | 0.5    0.6    0.5 |
+    // |         |         |
+    // |         v         |
+    // | 0.4 -> 0.7 <- 0.3 |
+    // |         ^         |
+    // |         |         |
+    // | 0.5    0.9    0.5 |
+
+    double tm = array[full_size * ((y * size) + size) + (x * size) + size / 2];
+    double rm = array[full_size * ((y * size) + size / 2) + (x * size) + size];
+    double bm = array[full_size * ((y * size) + size) + (x * size) + size / 2];
+    double lm = array[full_size * ((y * size) + size / 2) + (x * size) + size];
+
+    array[full_size * ((y * size) + size / 2) + (x * size) + size / 2] = ((tm + rm + bm + lm) / 4.0) + rand_h(r_depth + 1);
+
 }
 
-double** new_map(int N){
-    int WIDTH = (int) pow(2, N);
-
-    //double to_return[WIDTH + 1][WIDTH + 1];
-    double **to_return;
-
-    printf("Allocating memory...\n");
-    to_return = malloc((WIDTH + 1) * sizeof *to_return);
-    for (int i = 0; i < (WIDTH + 1); i++){
-        to_return[i] = malloc((WIDTH + 1) * sizeof *to_return[i]);
-    }
-
+double* new_map(int N){
+    // Create a terrain map based on a detail level.
+    
     srand(time(NULL));
 
-    to_return[0][0] = rd();
-    to_return[WIDTH][0] = rd();
-    to_return[WIDTH][WIDTH] = rd();
-    to_return[0][WIDTH] = rd();
+    // Width of the map is equal to 2 ^ N + 1 so there is always a middle
+    int WIDTH = (int) pow(2, N) + 1;
 
+    double *to_return;
+
+    // Allocate the memory to the array. even though this is going to be a
+    // 3D heightmap, I only store it in a 1D array. This means all the data
+    // is together by default and can still be accessed with normal x/y
+    // coordinates with simple multiplication.
+    printf("Allocating memory...\n");
+    printf("WIDTH = %d\n", WIDTH);
+    to_return = malloc((WIDTH) * (WIDTH) * sizeof *to_return);
+
+
+    // Set each of the corners. Ideally these would be random from 0 to 1,
+    // but this helps debug easier (since currently they are showing up much
+    // brighter than the others (so I at least know this code works).
+    to_return[0] = rd();
+    to_return[WIDTH - 1] = rd();
+    to_return[(WIDTH - 1) * WIDTH] = rd();
+    to_return[(WIDTH - 1) * WIDTH + WIDTH - 1] = rd();
+
+    // Here we go through the loop, once for each detail level. At each
+    // detail level it adds more and more detail using the 'diamon-square'
+    // method (which is just named 'diamond' here).
     printf("Entering the loop\n");
-    for (int r = 0; r < N; r++){
+    for (int r = 0; r <= N; r++){
         printf("%d\n", r);
         for (int y = 0; y < pow(2, r); y++){
             for (int x = 0; x < pow(2, r); x++){
-                // printf("starting the diamond step, %d, %d, %d\n", r, x, y);
                 diamond(to_return, r, x, y, N);
             }
         }
     }
     printf("Done with the loop.\n");
 
+    // Normalize to bring all values between 0 and 1
+    printf("Normalizing...\n");
+    double max = -1000.0;
+    double min = 1000.0;
+    for (int y = 0; y < WIDTH; y++){
+        for (int x = 0; x < WIDTH; x++){
+            #ifdef DEBUG
+            printf("X: %d, Y: %d, Val: %.2f\n", x, y, to_return[y * WIDTH + x]);
+            #endif
+
+            if (to_return[y * WIDTH + x] > max){
+                max = to_return[y * WIDTH + x];
+            }
+            else if (to_return[y * WIDTH + x] < min){
+                min = to_return[y * WIDTH + x];
+            }
+        }
+    }
+
+    for (int y = 0; y < WIDTH; y++){
+        for (int x = 0; x < WIDTH; x++){
+            to_return[y * WIDTH + x] = (to_return[y * WIDTH + x] - min) / (max - min);
+        }
+    }
+    printf("MIN: %.2f, MAX: %.2f\n", min, max);
+
+    printf("Done Normalizing.\n");
+
+    // Return the final array.
     return to_return;
+}
+
+int main() {
+    for (int i = 0; i < 10; i++){
+        double* a = new_map(9);
+    }
+    return 0;
 }
--- mid_dis.py
+++ mid_dis.py
@@ -1,79 +1,142 @@
 import sys
 import ctypes
 import pygame
+import math
+
 pygame.init()
 
+# Detail level. Map will be (N ^ 2 + 1) x (N ^ 2 + 1).
+N = 9
+
 def get_func():
+    # Load the dll file containing the function
     nDLL = ctypes.CDLL("mid_dis_dll.dll")
 
-    N = 8 # 2^9 = 512. Creates a 513 x 513 map
+    # Load the function and let python know what the datatypes are.
     func = nDLL['new_map']
-    func.restype = ctypes.POINTER(ctypes.c_double * (2 ** N + 1) * (2 ** N + 1))
-    #func.restype = ctypes.c_double * (2 ** N + 1) * (2 ** N + 1)
+    func.restype = ctypes.POINTER(ctypes.c_double * int((2 ** N + 1) ** 2))
     func.argtype = ctypes.c_int
 
     return func
 
-def run():
-    N = 9
+def draw_map():
+    """The main function. Calls the function and creates an image
+    based on the data.
+    """
+
     func = get_func()
     array = func(N)
 
-    print(array)
-    width = len(array[0])
+    width = int(math.sqrt(len(array[0])))
+    print(width)
+    
+    # This outer surface will just be a red border used for debugging (making
+    # it easier to see in the window's image viewer).
+    outer_surf = pygame.Surface((width + 2, width + 2))
+    outer_surf.fill((255, 0, 0))
 
+    # The surf on which to draw the actual data.
     surf = pygame.Surface((width, width))
 
+    # Almost sure all the below python is working as intended.
     print("locking")
     surf.lock()
     print("going into the loop")
     for y in xrange(width):
         for x in xrange(width):
-            print(y, x)
-            try:
-                clr = max(min(int(array[0][x][y] * 256), 255), 0)
-            except ValueError:
-                print array[0][x][x]
-                clr = 0
-            except OverflowError:
-                print array[0][x][y]
-                clr = 255
 
-            surf.set_at((x, y), (clr, clr, clr))
+            clr = int(array[0][y * width + x] * 256)
+
+            clr = max(min(clr, 255), 0) # Limit 0 <= clr <= 255 for pygame
+
+            # Add in some colors to make it look more like a gameworld
+            if clr < 65:
+                base_color = (65, 105, 225)
+                color = lerp_color(gamma_color(base_color, 0.3),
+                                   gamma_color(base_color, 0.7),
+                                   clr / 256.)
+            elif clr < 100:
+                base_color = (46, 139, 87)
+                color = lerp_color(gamma_color(base_color, 0.3),
+                                   gamma_color(base_color, 0.7),
+                                   clr / 256.)
+            elif clr < 150:
+                base_color = (131, 139, 131)
+                color = lerp_color(gamma_color(base_color, 0.3),
+                                   gamma_color(base_color, 0.7),
+                                   clr / 256.)
+            else:
+                color = (clr, clr, clr)
+
+            surf.set_at((x, y), color)
+
     print("done with the loop")
     surf.unlock()
     print("unlocked")
 
-    pygame.image.save(surf, "mid_dis.png")
+    outer_surf.blit(surf, (1, 1))
+
+    # switch these comments if just saving the file
+    #pygame.image.save(outer_surf, "mid_dis.png")
+    return surf
+
+def run():
+    """ This just creates a window that shows the rendered
+    image. You can press SPACE to get a new map.
+    """
+
+    screen_size = (513, 513)
+    screen = pygame.display.set_mode(screen_size)
+
+    surf = draw_map()
+    screen.blit(surf, (0, 0))
+
+    done = False
+    while not done:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                done = True
+                break
+
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_ESCAPE:
+                    done = True
+                    break
+
+                if event.key == pygame.K_SPACE:
+                    surf = draw_map()
+                    screen.blit(surf, (0, 0))
+
+        pygame.display.flip()
+
+    pygame.quit()
+
+# -- HELPER METHODS --
+
+def lerp(a, b, t):
+    return (1 - t) * a + t * b
+
+def lerp_color(clr1, clr2, t):
+    return (lerp(clr1[0], clr2[0], t),
+            lerp(clr1[1], clr2[1], t),
+            lerp(clr1[2], clr2[2], t))
+
+def gamma_color(clr1, t):
+    if t < 0.5:
+        return lerp_color((0, 0, 0), clr1, t * 2)
+    else:
+        return lerp_color(clr1, (255, 255, 255), (t - 0.5) * 2)
 
 def trace(frame, event, arg):
-    print "%s, %s:%d" % (event, frame.f_code.co_filename, frame.f_lineno)
+    """Used to track the program during a segmentation fault (since you
+    can't write an exception for it).
+    """
+
+    #print "%s, %s:%d" % (event, frame.f_code.co_filename, frame.f_lineno)
+    print("%d" % frame.f_lineno),
     return trace
 
-sys.settrace(trace)
-run()
-
-"""
-import pygame
-pygame.init()
-surf = pygame.Surface((width, width))
-
-print("locking")
-surf.lock()
-print("going into the loop")
-for y in xrange(width):
-    print("y=%d"%y)
-    for x in xrange(width):
-        print ("x=%d"%x)
-        try:
-            clr = max(min(int(array[1][x][y] * 256), 255), 0)
-        except ValueError:
-            print array[0][y][x]
-            clr = 0
-        surf.set_at((x, y), (clr, clr, clr))
-print("done with the loop")
-surf.unlock()
-print("unlocked")
-
-pygame.image.save(surf, "mid_dis.png")
-"""
+if __name__ == "__main__":
+    #sys.settrace(trace) # Uncomment for debugging purposes.
+    #draw_map() # Switch comments if just saving the image.
+    run()
